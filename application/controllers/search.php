148030cb9c449a37d863fca3df50c7469dbdd23c
SivaCoHan <sivacohan@gmail.com>
2012-12-04 19:40:03 +0800

    add include folder

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -1,7 +1,1 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');

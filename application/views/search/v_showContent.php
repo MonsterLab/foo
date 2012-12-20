8c0a8d59dfded2160a6f77f2b8204438b5c8d115
fei <zcloudsky@sina.com>
2012-12-20 10:46:01 +0800

    view

--- application/views/search/v_showContent.php
+++ application/views/search/v_showContent.php
@@ -4,7 +4,7 @@
     <a href="<?php echo base_url("search/showUserInfos/{$uid}")?>"><input type="button" value="返回"/></a>
 </div>
         <br/>
-<div id="content">
+<div id="">
     <?php
         echo "标题：{$title}<br>";
         echo "内容：{$content}<br>";
